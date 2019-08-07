0adf6185e132b24c06682923817bda8e7387ec11
John Guest <jcguest90@gmail.com>
2019-08-07 18:24:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -43,8 +43,22 @@ end
 
 def play_method(songs)
   puts "Please enter a song name or number:"
-   song_choice = gets.strip
-   
-   puts 
+   song_name = gets.strip
+   if song_name == (1...9)
+   puts "Playing <song_name>"
+    else 
+      puts "Invalid input, please try again"
+end
+
+
+def exit 
+  puts "Goodbye"
+end 
+
+def run
+  help_method
+  puts "Please enter a command"
+  user_input = gets.strip
+  
    
   
